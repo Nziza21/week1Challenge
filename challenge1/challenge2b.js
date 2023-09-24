70ef8f0e6b742e8a1452e4f81bd449842809dc24
Nziza21 <144722415+Nziza21@users.noreply.github.com>
2023-09-24 22:11:44 +0300

    Update challenge2b.js
    Implement javascript program for speed detector logic

--- challenge1/challenge2b.js
+++ challenge1/challenge2b.js
@@ -7,6 +7,7 @@ function calculateDemeritPoints(speed) {
     return 'Ok';
   } else {
     const demeritPoints = Math.floor((speed - speedLimit) / kmPerDemeritPoint);
+    
     if (demeritPoints <= maxDemeritPoints) {
       return `Points: ${demeritPoints}`;
     } else {
@@ -14,10 +15,19 @@ function calculateDemeritPoints(speed) {
     }
   }
 }
+
+// Prompt the user for input
 const userInput = prompt("Enter the car's speed (in km/h):");
+
+// Convert the user input to a number
 const carSpeed = parseFloat(userInput);
+
+// Check if the input is a valid number
 if (!isNaN(carSpeed)) {
+  // Calculate and display demerit points or "Ok" message
   const result = calculateDemeritPoints(carSpeed);
   alert(result);
+} else {
+  // Display an error message for invalid input
   alert('Invalid input. Please enter a valid speed (in km/h).');
 }
